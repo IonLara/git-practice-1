f7cc0d8d8cb5d47c27958929da502fb84cf253cc
SebasMac <ionrodriguezlara@gmail.com>
2023-02-24 09:26:14 -0800

    Added some text

--- main.swift
+++ main.swift
@@ -2,8 +2,9 @@ print("Hello, Swift!")
 print("Hello, Swift!")
 print("Hello, Swift!")
 
+var text = "This is a change"
 var a = 10
 var b = 10
 var c = 10
 print("a + b + c = \(a + b + c)")
-
+print(text)
